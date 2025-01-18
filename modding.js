06991f335794d3ce9d4722321b55a116dd94e1f3
joshegatito <148405949+joshegatito@users.noreply.github.com>
2025-01-18 18:16:33 -0500

    Add files via upload

--- modding.js
+++ modding.js
@@ -1,16 +1,32 @@
+// creditos por las imagenes a los autores TheKingofWarrr, ClezylGabane, urbanator
 const imagenes = [
     "./images/blender.png",
     "./images/3ds max.png"
 ];
 
+const backgrounds = [
+    "./images/movies/banner.jpg",
+    "./images/movies/banner2.jpg",
+    "./images/movies/banner3.jpg",
+    "./images/movies/banner4.jpg"
+];
+
 let foto = 0;
 
 function nextimage() {
-    foto = (foto + 1) % imagenes.length;
+    foto = (foto + 1) % Math.max(imagenes.length, backgrounds.length);
+
     const cambiar = document.querySelectorAll('.movie-title');
-    cambiar.forEach(img => {
-        img.src = imagenes[foto];
+    cambiar.forEach((img, index) => {
+        if (imagenes[index % imagenes.length]) {
+            img.src = imagenes[foto % imagenes.length];
+        }
     });
+
+    const banner = document.querySelector('.banner');
+    if (banner) {
+        banner.style.backgroundImage = `url(${backgrounds[foto % backgrounds.length]})`;
+    }
 }
 
 setInterval(nextimage, 3000);
